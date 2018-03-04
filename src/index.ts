1fa699eb7d83ecb7ff1f170f9b506c338b9582be
Andreas Neumann <andy@neumann-home.net>
2018-03-04 18:11:47 +0100

    create index file for exporting lib

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { default as renderClient } from "./renderClient";
+export { default as renderServer } from "./renderServer";
+export { default as ChildRoutes } from "./components/ChildRoutes";
+export { default as Page } from "./components/Page";
+export { default as PageRouter } from "./components/PageRouter";
